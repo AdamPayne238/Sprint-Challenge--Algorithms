dac60e03327a13e2ad3683c6cbd91560f2275565
Adam Payne <ajpayne238@gmail.com>
2020-02-21 13:33:57 -0500

    passing all tests

--- Short-Answer/Algorithms_Answers.md
+++ Short-Answer/Algorithms_Answers.md
@@ -3,13 +3,21 @@
 ## Exercise I
 
 a)
-
+O(n^3)
 
 b)
-
+O(n^2)
 
 c)
+O(n-1)
 
 ## Exercise II
 
+for every floor possible from floor zero to length of floors vertically
+if the floor we are on is greater than or equal to f
+the egg will break at this floor so we will not drop the egg
+otherwise if the floor is less than f we will drop the egg
+this will reduce the amount of eggs broken because we will not drop from 
+floors which we know the egg will break and will not drop from higher than that floor
+because the egg will surely break.
 
--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,8 +3,33 @@ Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
+
+word = "abcthxyzth"
+
+
 def count_th(word):
-    
-    # TBC
-    
-    pass
+    # set count to 0
+    count = 0
+
+    # base case
+    if len(word) <= 1:
+        return 0
+        print("len(word", len(word))
+
+    elif "th" in word:
+        # index of first and second letters
+        if word[0] == 't' and word[1] == 'h':
+            print(count)
+            count += 1
+            print("word[0]", word[0])
+            print("word[1]", word[1])
+
+    # start at 1st letter and iterate through with count
+    print(count_th(word[1:]) + count)
+    return count_th(word[1:]) + count
+
+
+print(len(word))
+print(word[0])
+print(word[1])
+count_th(word)
